9d7409be95e27f37ab1418fd0ddc3f9d56b404d1
Allan Nascimento <alcristi.sto@gmail.com>
2025-02-06 19:43:49 -0300

    config/instance of service and repository in injector and set routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,14 +1,15 @@
 import { Express, Router } from 'express';
 import { di } from '../di';
-import TestController from '../controllers/test.controller';
-import TestService from '../services/test.service';
+import { AuthController } from '../controllers/auth.controller';
+import { AuthService } from '../services/auth.service';
 
 const router = Router();
 const prefix = '/api';
 
 export default (app: Express) => {
-  app.use(
-    prefix,
-    new TestController(router, di.getService(TestService)).router
+  app.use('/', new AuthController(router, di.getService(AuthService)).router);
+
+  app.use(prefix, AuthController.authenticate, (req, res) =>
+    res.json({ test: 'logado' })
   );
 };
