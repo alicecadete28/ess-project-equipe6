f84c3f5ca0557a2a438addf2aba5945522d2e8e2
Allan Nascimento <alcristi.sto@gmail.com>
2025-02-16 15:33:30 -0300

    refactor/separação das features no diretorio de feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,6 @@ import app from './app';
 import logger from './logger';
 import Env from './env';
 
-
 app.listen(Env.PORT, () => {
   logger.info(`Server started on http://localhost:${Env.PORT}/api`);
 });
-
-
