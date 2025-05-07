75e39acf292c42628b692f2f7ead8308117a2a89
Alice Cadete <abc3@cin.ufpe.br>
2025-05-07 15:11:24 -0300

    Revert "Update next.config.js"
    This reverts commit f1ccebbd1eed89afebce266547e2fb538daa3137.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
-    unoptimized: true, // necessário para exportação estática
-    domains: ["picsum.photos"],
+    domains: ['picsum.photos'],
   },
-  basePath: "/ess-project-equipe6", // obrigatório para GitHub Pages
 };
 
 export default nextConfig;
