f1ccebbd1eed89afebce266547e2fb538daa3137
Alice Buarque <113849722+alicecadete28@users.noreply.github.com>
2025-05-06 22:19:11 -0300

    Update next.config.js
    so pra criar uma pagina

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
-    domains: ['picsum.photos'],
+    unoptimized: true, // necessário para exportação estática
+    domains: ["picsum.photos"],
   },
+  basePath: "/ess-project-equipe6", // obrigatório para GitHub Pages
 };
 
 export default nextConfig;
